9cd85b0645ceece78428183d15cf7b3cfb71e21c
Wesley <wluu03@calpoly.edu>
2021-02-04 18:55:58 -0800

    Added headers

--- ordered_list.py
+++ ordered_list.py
@@ -25,6 +25,7 @@ class OrderedList:
            DO NOT have an attribute to keep track of size'''
         self.head = None
 
+    # Returns True if OrderedList is empty
     def is_empty(self):
         '''Returns True if OrderedList is empty
             MUST have O(1) performance'''
@@ -32,6 +33,10 @@ class OrderedList:
             return True
         return False
 
+    # Any data type ---> Bool
+    # Adds an item to OrderedList, in the proper location based on ordering of items
+    #from lowest (at head of list) to highest (at tail of list) and returns True.
+    #If the item is already in the list, do not add it again and return False.
     def add(self, item):
         '''Adds an item to OrderedList, in the proper location based on ordering of items
            from lowest (at head of list) to highest (at tail of list) and returns True.
@@ -65,6 +70,9 @@ class OrderedList:
 
             current = current.next
 
+    # Any data type ---> Bool
+    # Removes the first occurrence of an item from OrderedList. If item is removed (was in the list) 
+    # returns True.  If item was not removed (was not in the list) returns False
     def remove(self, item):
         '''Removes the first occurrence of an item from OrderedList. If item is removed (was in the list) 
           returns True.  If item was not removed (was not in the list) returns False
@@ -96,6 +104,9 @@ class OrderedList:
             current = current.next
         return False
   
+    # Any data type ---> Integer or None
+    # Returns index of the first occurrence of an item in OrderedList (assuming head of list is index 0).
+    # If item is not in list, return None
     def index(self, item):
         '''Returns index of the first occurrence of an item in OrderedList (assuming head of list is index 0).
            If item is not in list, return None
@@ -115,6 +126,9 @@ class OrderedList:
             index += 1
             current = current.next
 
+    # Integer ---> Node.item
+    # Removes and returns item at index (assuming head of list is index 0).
+    # If index is negative or >= size of list, raises IndexError
     def pop(self, index):
         '''Removes and returns item at index (assuming head of list is index 0).
            If index is negative or >= size of list, raises IndexError
@@ -135,6 +149,8 @@ class OrderedList:
         self.remove(item)
         return item
 
+    # Any data type ---> Bool
+    # Searches OrderedList for item, returns True if item is in list, False otherwise
     def search(self, item):
         '''Searches OrderedList for item, returns True if item is in list, False otherwise"
            To practice recursion, this method must call a RECURSIVE method that
@@ -156,6 +172,7 @@ class OrderedList:
             self.head = current
             return recur
 
+    # Return a Python list representation of OrderedList, from head to tail
     def python_list(self):
         '''Return a Python list representation of OrderedList, from head to tail
            For example, list with integers 1, 2, and 3 would return [1, 2, 3]
@@ -172,6 +189,7 @@ class OrderedList:
             current = current.next
 
     
+    # Return a Python list representation of OrderedList, from tail to head, using recursion
     def python_list_reversed(self):
         '''Return a Python list representation of OrderedList, from tail to head, using recursion
            For example, list with integers 1, 2, and 3 would return [3, 2, 1]
@@ -194,6 +212,7 @@ class OrderedList:
         return recur
 
     
+    # Returns number of items in the OrderedList
     def size(self):
         '''Returns number of items in the OrderedList
            To practice recursion, this method must call a RECURSIVE method that
